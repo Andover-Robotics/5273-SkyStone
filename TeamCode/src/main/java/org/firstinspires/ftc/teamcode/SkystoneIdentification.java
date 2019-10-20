4927066bfe49e8647ab05b5d34c13a32a611bcdb
Daniel Ivanovich <dan@ivanovi.ch>
2019-10-20 02:17:23 -0400

    Revamp Vuforia code to track navigation targets and the calculate robot's position and rotation
    Also obtained new Vuforia license key and placed it in the SD card of the phone instead of the code or repository itself to keep the key secure while avoiding complications for newer programmers.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneIdentification.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneIdentification.java
@@ -1,55 +1,189 @@
-/* Copyright (c) 2019 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
+import android.os.Environment;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
 
-import org.firstinspires.ftc.teamcode.vision.NavigationTargetTracking;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
-@TeleOp(name = "SKYSTONE Vuforia Nav Demo", group = "Demos")
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Autonomous(name = "Vuforia Demo", group = "Test")
 public class SkystoneIdentification extends LinearOpMode {
 
+    // Used to convert between inches and millimeters ([value in in.] * mmPerInch = [value in mm.])
+    private static final float mmPerInch = 25.4f;
 
-    @Override
-    public void runOpMode() {
-        NavigationTargetTracking targetTracking = new NavigationTargetTracking(0, 0, 0, telemetry, hardwareMap);
+    // Fields for Vuforia
+    private VuforiaTrackables visionTargets;
+    private VuforiaLocalizer vuforiaLocalizer;
+    private VuforiaLocalizer.Parameters parameters;
+
+    // Stores all the initialized VuforiaTrackables to check for detected markers
+    private ArrayList<VuforiaTrackable> allTrackables;
+
+    // The last known location of the bot, obtained from Vuforia
+    // The phone's offset from the center of the bot with the long side along the y axis and screen up
+    private OpenGLMatrix lastKnownLocation, phoneLocation;
+
+    private float robotX = 0, robotY = 0, robotAngle = 0;
+
+    public void runOpMode() throws InterruptedException {
+        setupVuforia();
+
+        // We don't know where the robot is, so set it to the origin
+        // If we don't include this, it would be null, which would cause errors later on
+        lastKnownLocation = originMatrix();
+
+        telemetry.addLine("Ready!");
+        telemetry.update();
 
         waitForStart();
 
-        targetTracking.activate();
+        // Start tracking the targets
+        visionTargets.activate();
 
-        while (!isStopRequested()) {
-            NavigationTargetTracking.NavigationTargetReturn targetReturn = targetTracking.checkForTargets();
+        while (opModeIsActive()) {
+            for (VuforiaTrackable trackable : allTrackables) {
+                VuforiaTrackableDefaultListener listener = (VuforiaTrackableDefaultListener) trackable.getListener();
+
+                if (listener.isVisible()) {
+                    // Ask the listener for the latest information on where the robot is
+                    OpenGLMatrix latestLocation = listener.getUpdatedRobotLocation();
+
+                    // The listener will sometimes return null, so we check for that to prevent errors
+                    if (latestLocation != null)
+                        lastKnownLocation = latestLocation;
+
+                    float[] coordinates = lastKnownLocation.getTranslation().getData();
+
+                    robotX = coordinates[0];
+                    robotY = coordinates[1];
+                    robotAngle = Orientation.getOrientation(lastKnownLocation, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+
+                    // Send information about whether the target is visible, and where the robot is
+                    telemetry.addData("Tracking " + trackable.getName(), listener.isVisible());
+                    telemetry.addData("Last Known Location", formatMatrix(lastKnownLocation));
+                    break;
+                }
+            }
+
+            // Send telemetry and idle to let hardware catch up
+            telemetry.update();
+            idle();
+        }
+    }
+
+    private void setupVuforia() throws InterruptedException {
+        // Read VUFORIA_KEY from SD Card file for the security of the new developer key
+        File sdcard = Environment.getExternalStorageDirectory();
+
+        //Get the text file
+        File file = new File(sdcard, "FIRST/Vuforia_Key_2019_2020.txt");
+
+        //Read text from file
+        StringBuilder vuforiaKey = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                vuforiaKey.append(line);
+                vuforiaKey.append('\n');
+            }
+            br.close();
+        } catch (IOException e) {
+            throw new InterruptedException("Please store the Vuforia key on the phone's SD card in the FIRST folder and make sure the file name reflects the one used in the code!");
         }
 
-        targetTracking.deactivate();
+        telemetry.addLine("NOTICE: If no camera view is visible on the robot controller, the Vuforia key is missing from the SD card!");
+        telemetry.update();
+
+        // Setup parameters to create localizer
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = vuforiaKey.toString();
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parameters.useExtendedTracking = false;
+        vuforiaLocalizer = ClassFactory.getInstance().createVuforia(parameters);
+
+        // These are the vision targets that we want to use
+        visionTargets = vuforiaLocalizer.loadTrackablesFromFile("/sdcard/FIRST/Skystone");
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
+
+
+        // Set phone location on robot (from center with screen facing up)
+        phoneLocation = createMatrix(0, 0, 0, 90, 0, 0);
+
+        // Initialize the VuforiaTrackable objects and stores them in allTrackables
+        allTrackables = new ArrayList<>();
+
+        initTrackable(0, "Stone Target", originMatrix());
+        initTrackable(1, "Blue Rear Bridge", originMatrix());
+        initTrackable(2, "Red Rear Bridge", originMatrix());
+        initTrackable(3, "Red Front Bridge", originMatrix());
+        initTrackable(4, "Blue Front Bridge", originMatrix());
+        initTrackable(5, "Red Perimeter 1", originMatrix());
+        initTrackable(6, "Red Perimeter 2", originMatrix());
+        initTrackable(7, "Front Perimeter 1", originMatrix());
+        initTrackable(8, "Front Perimeter 2", originMatrix());
+        initTrackable(9, "Blue Perimeter 1", createMatrix(0, 36 * mmPerInch, 0, 90, 0, 90));
+        initTrackable(10, "Blue Perimeter 2", originMatrix());
+        initTrackable(12, "Rear Perimeter 1", originMatrix());
+        initTrackable(11, "Rear Perimeter 2", originMatrix());
+    }
+
+    /* Creates a matrix for determining the locations and orientations of objects
+    // Units are millimeters for x, y, and z, and degrees for u, v, and w
+    // u is rotation on the x axis, v on the y axis, and w on the z axis
+    // The right hand rule should be utilized in keeping dimensions in check
+    // The rotations are applied in the following order: x, y, z axes
+    // This is crucial to know because the order can greatly alter your final result
+    // Once again, use the right hand rule and visualize the rotation of the object
+    */
+    private OpenGLMatrix createMatrix(float x, float y, float z, float u, float v, float w) {
+        return OpenGLMatrix.translation(x, y, z).
+                multiplied(Orientation.getRotationMatrix(
+                        AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, u, v, w));
+    }
+
+    // Creates a matrix with 0 for all values
+    // On the field, this represents the bottom left corner of the field from the audience's view
+    // This is the corner with the red depot (the red square) by the human player station
+    private OpenGLMatrix originMatrix() {
+        return createMatrix(0, 0, 0, 0, 0, 0);
+    }
+
+    // Formats a matrix into a readable string
+    private String formatMatrix(OpenGLMatrix matrix) {
+        return matrix.formatAsTransform();
+    }
+
+    private void initTrackable(int targetIndex, String name, OpenGLMatrix location) {
+        VuforiaTrackable trackable = visionTargets.get(targetIndex);
+        trackable.setName(name);
+        trackable.setLocation(location);
+
+        // Setup listener and inform it of phone information
+        ((VuforiaTrackableDefaultListener) trackable.getListener()).setPhoneInformation(phoneLocation, parameters.cameraDirection);
+
+        allTrackables.add(trackable);
     }
 }
