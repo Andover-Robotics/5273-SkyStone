c537265bae0b418d7899e2fb890a5bffa6ad29d9
Vincent X <vincentxu04@gmail.com>
2019-11-20 20:23:45 -0500

    Revert "Merge branch 'master' of https://github.com/Andover-Robotics/5273-SkyStone"
    This reverts commit 031c7cf8e33bc7e5b005662919f16840784b5389, reversing
    changes made to 9ece7dae75ac3ec065b71a874d7ef949c5db33ee.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/MecanumTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/MecanumTestTeleOp.java
@@ -6,14 +6,12 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TeleOp", group = "Test")
-public class MecanumTestTeleOp extends OpMode {
+public class  MecanumTestTeleOp extends OpMode {
 
     private MecanumDrive mecanumDrive;
     private DcMotor motorFL, motorFR, motorBL, motorBR;
-    private Servo foundationServo;
 
     @Override
     public void init() {
@@ -22,11 +20,8 @@ public class MecanumTestTeleOp extends OpMode {
         motorBL = hardwareMap.dcMotor.get("motorBL");
         motorBR = hardwareMap.dcMotor.get("motorBR");
 
-        foundationServo = hardwareMap.servo.get("foundationMover");
-        foundationServo.setPosition(0);
-
-        motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
 
         mecanumDrive = MecanumDrive.fromOctagonalMotors(motorFL, motorFR, motorBL, motorBR, this, 63, 5200);
     }
@@ -36,10 +31,5 @@ public class MecanumTestTeleOp extends OpMode {
         Coordinate strafe = Coordinate.fromXY(-gamepad1.left_stick_x, gamepad1.left_stick_y);
         mecanumDrive.setStrafe(strafe);
         mecanumDrive.setRotationPower(-gamepad1.right_stick_x);
-
-        if (gamepad1.left_bumper)
-            foundationServo.setPosition(0);
-        else if(gamepad1.right_bumper)
-            foundationServo.setPosition(0.69);
     }
 }
