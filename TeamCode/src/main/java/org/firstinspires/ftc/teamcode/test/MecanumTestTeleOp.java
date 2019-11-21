fd3760597efb4f18c5def37e85d7da7c45b9dcb6
Daniel Ivanovich <dan@ivanovi.ch>
2019-11-20 19:07:38 -0500

    work on auto and teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/MecanumTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/MecanumTestTeleOp.java
@@ -6,12 +6,14 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TeleOp", group = "Test")
-public class  MecanumTestTeleOp extends OpMode {
+public class MecanumTestTeleOp extends OpMode {
 
     private MecanumDrive mecanumDrive;
     private DcMotor motorFL, motorFR, motorBL, motorBR;
+    private Servo foundationServo;
 
     @Override
     public void init() {
@@ -20,8 +22,11 @@ public class  MecanumTestTeleOp extends OpMode {
         motorBL = hardwareMap.dcMotor.get("motorBL");
         motorBR = hardwareMap.dcMotor.get("motorBR");
 
-        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
+        foundationServo = hardwareMap.servo.get("foundationMover");
+        foundationServo.setPosition(0);
+
+        motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
 
         mecanumDrive = MecanumDrive.fromOctagonalMotors(motorFL, motorFR, motorBL, motorBR, this, 63, 5200);
     }
@@ -31,5 +36,10 @@ public class  MecanumTestTeleOp extends OpMode {
         Coordinate strafe = Coordinate.fromXY(-gamepad1.left_stick_x, gamepad1.left_stick_y);
         mecanumDrive.setStrafe(strafe);
         mecanumDrive.setRotationPower(-gamepad1.right_stick_x);
+
+        if (gamepad1.left_bumper)
+            foundationServo.setPosition(0);
+        else if(gamepad1.right_bumper)
+            foundationServo.setPosition(0.69);
     }
 }
