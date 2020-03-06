30f03fc0095481781fa91258cc8c2a0554821a3a
Vincent X <vincentxu04@gmail.com>
2020-03-06 16:10:14 -0500

    Autonomous: Parking under the bridge only - against wall and against middle bridge

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/red/ParkTile3.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.autonomous.red;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.path.heading.ConstantInterpolator;
+
+import org.firstinspires.ftc.teamcode.GlobalConfig;
+import org.firstinspires.ftc.teamcode.autonomous.AutonomousMasterRoadrunner;
+import org.firstinspires.ftc.teamcode.autonomous.RobotAlliance;
+import org.firstinspires.ftc.teamcode.vision.SkystoneLocation;
+
+public class ParkTile3 extends AutonomousMasterRoadrunner {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode(); // Initializes and configures
+
+        checkForStop();
+
+        // Left side of bot starts from the right edge of tile 3
+        double startingPositionX = -24 + GlobalConfig.BOT_WIDTH_IN / 2;
+        double startingPositionY = 72 - GlobalConfig.BOT_LENGTH_IN / 2;
+
+        double bridgePositionY = 72 - GlobalConfig.BOT_LENGTH_IN / 2;
+
+        driveBase.setPoseEstimate(new Pose2d(new Vector2d(startingPositionX, startingPositionY), 0));
+        Pose2d underBridge = new Pose2d(new Vector2d(0, bridgePositionY), 0);
+
+        drive(bot -> bot.splineTo(underBridge));
+    }
+}
